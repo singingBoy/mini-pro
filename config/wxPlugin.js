8e23dfda607493bc3ef70f60d1976fb973f11046
singingBoy <737708900@qq.com>
2019-01-11 14:37:59 +0800

    2019年1月11日

--- config/wxPlugin.js
+++ config/wxPlugin.js
@@ -1,16 +1,26 @@
-import glob from 'glob';
-import {resolve, parse, join} from 'path';
 import fs from 'fs';
+import {resolve, parse, join} from 'path';
+import MultiEntryPlugin from "webpack/lib/MultiEntryPlugin";
 
 const pluginName = 'WxPlugin';
 export default class WxPlugin {
   constructor() {
+    this.chunkName = 'common.js';
   }
   apply(compiler) {
-    const { context } = compiler;
-    console.log(resolve(`${context}/src`));
+    const { context, plugins } = compiler;
+    const entries = allSrcFiles(context);
+    const multiEntryPlugin = new MultiEntryPlugin(context, entries, this.chunkName);
+    compiler.apply(multiEntryPlugin);
+
+    compiler.hooks.entryOption.tap("EntryOptionPlugin", (context, entry) => {
+      const entries = allSrcFiles(context);
+      const multiEntryPlugin = new MultiEntryPlugin(context, entries, this.chunkName);
+      multiEntryPlugin.apply(compiler);
+    });
+
     compiler.hooks.beforeRun.tapAsync(pluginName, (compilation, callback) => {
-      console.log(allSrcFiles(context))
+
     });
     compiler.hooks.run.tap(pluginName, compilation => {
       console.log('WxPlugin starting...');
--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -1,11 +1,33 @@
-import {resolve} from 'path';
+import fs from 'fs';
+import {resolve, parse, join} from 'path';
 import webpack from 'webpack';
 import CleanWebpackPlugin from 'clean-webpack-plugin';
+import MultiEntryPlugin from "webpack/lib/MultiEntryPlugin";
 
 import BuildUtils, {OUTPUT} from './config/config.util';
-import WxPlugin from './config/wxPlugin';
+
+const entries = allSrcFiles(__dirname);
+console.log(entries);
+
+const constructFileLoader = (ext) => {
+  console.log('=====>', `[name].${ext || '[ext]'}`);
+  return {
+    loader: 'file-loader',
+    options: {
+      outputPath: (url, resourcePath, context) => {
+        return resourcePath.replace(context, '').replace('src2', '');
+      },
+      name: `[name].${ext || '[ext]'}`,
+    },
+  }
+};
 
 export default {
+  entry: {
+    app: [
+      './src2/app.js',
+    ],
+  },
   output: {
     path: resolve(OUTPUT),
     publicPath: '/',
@@ -26,7 +48,7 @@ export default {
   },
   plugins: [
     new CleanWebpackPlugin([OUTPUT]),
-    new WxPlugin(),
+    new MultiEntryPlugin(__dirname, entries, ''),
     new webpack.HotModuleReplacementPlugin(),
   ],
   module: {
@@ -35,23 +57,29 @@ export default {
       loader: 'babel-loader',
       exclude: /node_modules/,
     }, {
+      test: /\.json$/,
+      use: [
+        constructFileLoader('json'),
+      ],
+    }, {
+      test: /\.wxml$/,
+      use: [
+        constructFileLoader('wxml'),
+      ],
+    }, {
       test: /\.less|.css$/,
-      include: resolve('src'),
       exclude: resolve('node_modules'),
-      use: [{
-        loader: 'file-loader',
-        options: {
-          name: '[name].wxss',
-        },
-      }, {
-        loader: 'less-loader',
-        options: {
-          sourceMap: process.env.NODE_ENV === 'development',
-          paths: [
-            resolve(__dirname, 'node_modules')
-          ]
-        }
-      }],
+      use: [
+        constructFileLoader('wxss'),
+        {
+          loader: 'less-loader',
+          options: {
+            sourceMap: process.env.NODE_ENV === 'development',
+            paths: [
+              resolve(__dirname, 'node_modules')
+            ]
+          }
+        }],
     }]
   },
   devServer: {
@@ -63,4 +91,26 @@ export default {
     aggregateTimeout: 300,
   },
   mode: process.env.NODE_ENV,
+}
+
+function allSrcFiles(context) {
+  const srcPath = resolve(`${context}/src2`);
+  const filesPath = [];
+
+  function traverseFiles(path) {
+    let files = fs.readdirSync(path);
+    files.forEach(item => {
+      let fPath = join(path, item);
+      let stat = fs.statSync(fPath);
+      if (stat.isDirectory() === true) {
+        traverseFiles(fPath);
+      }
+      if (stat.isFile() === true) {
+        filesPath.push(fPath);
+      }
+    });
+  }
+
+  traverseFiles(srcPath);
+  return filesPath;
 }
